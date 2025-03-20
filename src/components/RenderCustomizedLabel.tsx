b28919b95989d928da5992dbf1c7a0493c9ec176
Joaquin Palacios <joa_palacios@hotmail.com>
2025-03-21 10:24:21 +1100

    wind color speed udpated

--- src/components/RenderCustomizedLabel.tsx
+++ src/components/RenderCustomizedLabel.tsx
@@ -1,68 +1,24 @@
 import { LabelProps } from "recharts";
 
 interface CustomLabelProps extends LabelProps {
-  heightDifference?: number | null;
-  hasFaceWaveHeight?: boolean;
-  secondarySwellDirection?: number | null;
+  faceWaveHeight?: number;
+  secondarySwellDirection?: number;
 }
 
 const RenderCustomizedLabel = (props: CustomLabelProps) => {
-  const {
-    x,
-    y,
-    value,
-    fill,
-    heightDifference,
-    hasFaceWaveHeight,
-    secondarySwellDirection,
-  } = props;
-
-  console.log({ secondarySwellDirection });
+  const { x, y, value, fill, faceWaveHeight, secondarySwellDirection } = props;
 
   // Ensure y is a valid number before subtracting
-
-  const yPosition =
-    typeof y === "number" && !isNaN(y) && heightDifference
-      ? y - heightDifference
-      : typeof y === "number" && !isNaN(y)
-      ? y - 20
-      : 0;
+  const yPosition = typeof y === "number" && !isNaN(y) ? y - 20 : 0;
   const xPosition = typeof x === "number" && !isNaN(x) ? x : 0;
 
-  if (!hasFaceWaveHeight) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        y={yPosition}
-        x={xPosition}
-        height={16}
-        width={16}
-        fill={fill}
-      >
-        <path
-          d="M14.13 9.11h-12l6-7 6 7z"
-          transform={`rotate(${value}, 0, 0)`}
-          style={{
-            transformOrigin: "center",
-          }}
-        />
-        <path
-          d="M6.12 8h4v6h-4z"
-          transform={`rotate(${value}, 0, 0)`}
-          style={{
-            transformOrigin: "center",
-          }}
-        />
-      </svg>
-    );
-  } else {
+  if (secondarySwellDirection) {
     return (
       <g>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
-          y={yPosition - 36}
+          y={yPosition - 20}
           x={xPosition}
           height={16}
           width={16}
@@ -86,11 +42,11 @@ const RenderCustomizedLabel = (props: CustomLabelProps) => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
-          y={yPosition - 20}
+          y={yPosition}
           x={xPosition}
           height={16}
           width={16}
-          fill="#008a93"
+          fill={fill}
         >
           <path
             d="M14.13 9.11h-12l6-7 6 7z"
@@ -110,6 +66,33 @@ const RenderCustomizedLabel = (props: CustomLabelProps) => {
       </g>
     );
   }
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      y={yPosition}
+      x={xPosition}
+      height={16}
+      width={16}
+      fill={fill}
+    >
+      <path
+        d="M14.13 9.11h-12l6-7 6 7z"
+        transform={`rotate(${value}, 0, 0)`}
+        style={{
+          transformOrigin: "center",
+        }}
+      />
+      <path
+        d="M6.12 8h4v6h-4z"
+        transform={`rotate(${value}, 0, 0)`}
+        style={{
+          transformOrigin: "center",
+        }}
+      />
+    </svg>
+  );
 };
 
 export default RenderCustomizedLabel;
