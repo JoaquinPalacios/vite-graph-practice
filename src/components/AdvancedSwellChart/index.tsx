643e854eab25150bff9cd6cdfc4d53d6250e63ed
Joaquin Palacios <joa_palacios@hotmail.com>
2025-04-11 15:05:42 +1000

    Update index.tsx
    new arrow added on advance graph

--- src/components/AdvancedSwellChart/index.tsx
+++ src/components/AdvancedSwellChart/index.tsx
@@ -280,14 +280,12 @@ const AdvancedSwellChart = ({
               type="monotone"
               dataKey="height" // Plotting the 'height' property within eventData
               xAxisId={0} // Link to the main (hidden) time axis
-              name={eventId} // Optional: helpful for tooltips
+              name={eventId}
               stroke={color}
               fill="none"
               strokeWidth={2}
-              // dot={<SwellArrowDot />} // Pass data point via payload to dot
               // activeDot={false}
               connectNulls={false} // Show gaps if event disappears temporarily
-              // isAnimationActive={false}
               dot={(props: ProcessedDotProps) => {
                 console.log({ props });
                 const { cx, cy, payload, stroke } = props;
@@ -304,36 +302,29 @@ const AdvancedSwellChart = ({
 
                 // --- Calculate arrow properties ---
                 const baseSize = 8;
-                const size = baseSize + period * 0.5;
+                const size = baseSize + period * 1.25; // Scale size based on period
                 const clampedSize = Math.max(5, Math.min(16, size));
-                const rotation = direction;
+                const rotation = direction - 45;
 
                 return (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    y={cy - 20}
-                    x={cx}
+                    fill={stroke}
+                    strokeWidth={0}
+                    viewBox="0 0 256 256"
                     height={clampedSize}
                     width={clampedSize}
-                    fill={stroke}
+                    y={cy - clampedSize / 2}
+                    x={cx - clampedSize / 2}
+                    overflow="visible"
                   >
-                    <g>
-                      <path
-                        d="M14.13 9.11h-12l6-7 6 7z"
-                        transform={`rotate(${rotation}, 0, 0)`}
-                        style={{
-                          transformOrigin: "center",
-                        }}
-                      />
-                      <path
-                        d="M6.12 8h4v6h-4z"
-                        transform={`rotate(${rotation}, 0, 0)`}
-                        style={{
-                          transformOrigin: "center",
-                        }}
-                      />
-                    </g>
+                    <path
+                      transform={`rotate(${rotation}, 0, 0)`}
+                      style={{
+                        transformOrigin: "center",
+                      }}
+                      d="M184,216a8,8,0,0,1-8,8H80a8,8,0,0,1,0-16h96A8,8,0,0,1,184,216Zm45.66-101.66-96-96a8,8,0,0,0-11.32,0l-96,96A8,8,0,0,0,32,128H72v56a8,8,0,0,0,8,8h96a8,8,0,0,0,8-8V128h40a8,8,0,0,0,5.66-13.66Z"
+                    ></path>
                   </svg>
                 );
               }}
